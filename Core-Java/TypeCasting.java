6f039cd879d1d3375d7e9646596e0175a58343ea
himanshups <74396707+himanshups@users.noreply.github.com>
2025-04-08 14:37:30 +0530

    Update TypeCasting.java

--- Core-Java/TypeCasting.java
+++ Core-Java/TypeCasting.java
@@ -7,6 +7,11 @@ public class TypeCasting {
         float num2 = 18.34f;
         int b = (int) num2; // Explicit type casting from float to int;
         System.out.println(b); // 12.34 will be converted to 12;
+
+        // type promotion in Java
+        byte a = 123;
+        byte b = 127;
+        int result = a+b;
         System.out.println(num1);
     }
 }
